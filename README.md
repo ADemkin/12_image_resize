14300565f30b54d8418f1f38c8bb3c38eaf38b16
antondemkin <antondemkin@yandex.ru>
2017-07-31 18:45:05 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # Image Resizer
 
-[TODO. There will be project description]
+This script will resize your image.
+
+Anton Demkin, 2017
+
+# Installation
+The script requires Python 3. To install additional modules run this terminal command from folder where you put this script:
+```
+pip3 install -r requirements.txt
+```
+
+# Usage
+
+To run this script, use terminal:
+```
+$ python3 image_resize.py [arguments]
+```
+
+####Available arguments:
+
+-h --help: get help
+
+#####Required arguments:
+
+[path] - full path or filename of image you want to resize
+
+#####Optional arguments:
+
+-s --scale: scale ratio. Can not be used with -x or -y arguments
+
+-x --x_size: width of resized image
+
+-y --y_size: height of resized image
+
+-o --output: resized image will be placed at desired location 
+
+# Example
+
+```
+$ image_resize.py img.jpg -s 2
+File succesfully resized: /Users/antondemkin/PycharmProjects/devman/12_image_resize/img__1778x1008.jpg
+
+$ image_resize.py img.jpg -x 100 -y 300 -o ~/Desktop
+Warning, aspect ratio changed!
+File succesfully resized: /Users/antondemkin/Desktop/img__100x300.jpg
+```
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,94 @@
+from PIL import Image
+import argparse
+import os
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def check_if_aspect_changed(x1, y1, x2, y2):
+    # in some cases very small difference is aspect ratio is inevitable
+    # so it is a good idea to leave some gap allowed (50:60 vs 5005:6009)
+    aspect_difference = abs((x1 / y1) - (x2 / y2))
+    if aspect_difference > 0.0015:
+        return True
+    else:
+        return False
+
+
+def get_new_size(original_size, width=None, height=None, scale=None):
+    if scale is not None:
+        new_x, new_y = original_size[0] * scale, original_size[1] * scale
+    
+    elif (width and height) is not None:
+        new_x, new_y = width, height
+        if check_if_aspect_changed(original_size[0], original_size[1], new_x, new_y):
+            print("Warning, aspect ratio changed!")
+    
+    elif width is not None:
+        new_scale = original_size[0] / width
+        new_height = original_size[1] // new_scale
+        new_x, new_y = width, new_height
+    
+    elif height is not None:
+        new_scale = original_size[1] / height
+        new_width = original_size[0] // new_scale
+        new_x, new_y = new_width, height
+    else:
+        new_x, new_y = (original_size)
+    
+    return (int(new_x), int(new_y))
+
+
+def resize_image(input_path, output_path, width, heigh, scale):
+    try:
+        image = Image.open(input_path, 'r')
+    except IOError as error:
+        print("{}: {}".format(error.strerror, input_path))
+    else:
+        new_size = get_new_size(image.size, width, heigh, scale)
+        resized_image = image.resize(new_size)
+        pathname, extension = os.path.splitext(output_path)
+        new_path = pathname + "__{}x{}".format(new_size[0], new_size[1]) + extension
+        try:
+            resized_image.save(new_path)
+        except PermissionError as error:
+            print(error.strerror)
+        except IOError as error:
+            print("Cannot convert: {}".format(error))
+        else:
+            print("File succesfully resized: {}".format(new_path))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='This is a simple utility to scale images.',
+                                     epilog='Scale cannot be used with width or height.')
+    
+    parser.add_argument('input', help='path to image file', metavar='[/path/to/your/file]')
+    
+    group0 = parser.add_argument_group('output path')
+    group1 = parser.add_argument_group('width and height')
+    group2 = parser.add_argument_group('scale')
+    
+    group0.add_argument('-o', '--output', help='folder, where result image will be placed')
+    group1.add_argument('-x', '--x_size', metavar='640', help='determines height of new image', type=int)
+    group1.add_argument('-y', '--y_size', metavar='480', help='determines width of new image', type=int)
+    group2.add_argument('-s', '--scale', metavar='1.6', help='determines a new image scale', type=float)
+    
+    args = parser.parse_args()
+    
+    if args.scale is not None and (args.x_size or args.y_size) is not None:
+        print('Scale cannot be used with width or height arguments.\nUse only [-s] or [-x, -y]')
+    if args.output is None:
+        args.output = os.path.dirname(os.path.abspath(args.input))
+    
+    input_path = os.path.abspath(args.input)
+    filename = os.path.basename(input_path)
+    output_path = os.path.join(os.path.expanduser(args.output), filename)
+    
+    resize_image(input_path=input_path,
+                 output_path=output_path,
+                 width=args.x_size,
+                 heigh=args.y_size,
+                 scale=args.scale)
 
 
 if __name__ == '__main__':
-    pass
+    main()
